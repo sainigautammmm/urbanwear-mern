d0aa8c64fc38e925df68f9dc8c6bfe003e74c841
gautam saini <126060062+gautam7772@users.noreply.github.com>
2026-09-14 21:35:39 +0530

    Fix production API URL

--- FRONTEND/v/src/REDUX/ACTION/products.js
+++ FRONTEND/v/src/REDUX/ACTION/products.js
@@ -1,26 +1,69 @@
+// import axios from "axios";
+// axios.defaults.withCredentials = true;
+
+// import { FETCH_PRODUCT_FAILURE, FETCH_PRODUCT_REQUEST, FETCH_PRODUCT_SUCCESS } from "../Constant/constants";
+
+// export const fetchproductaction = () => async (dispatch) => {
+//   dispatch({ type: FETCH_PRODUCT_REQUEST });
+  
+//   try {
+//     const response = await axios.get("http://localhost:8000/api/products/all-products"); // Change to GET request for all products
+
+//     if (response.data.success) {
+//       dispatch({
+//         type: FETCH_PRODUCT_SUCCESS,
+//         payload: {
+//           Data: response.data.data, // Assuming `data` contains the list of products
+//           message: response.data.message,
+//         },
+//       });
+//     }
+//   } catch (error) {
+//     console.error(error);
+//     const message = error.response?.data?.message || "An error occurred"; // Access the error message safely
+//     dispatch({ type: FETCH_PRODUCT_FAILURE, payload: message });
+//   }
+// };
+
+
+
+```js
 import axios from "axios";
 axios.defaults.withCredentials = true;
 
-import { FETCH_PRODUCT_FAILURE, FETCH_PRODUCT_REQUEST, FETCH_PRODUCT_SUCCESS } from "../Constant/constants";
+import {
+  FETCH_PRODUCT_FAILURE,
+  FETCH_PRODUCT_REQUEST,
+  FETCH_PRODUCT_SUCCESS
+} from "../Constant/constants";
 
 export const fetchproductaction = () => async (dispatch) => {
   dispatch({ type: FETCH_PRODUCT_REQUEST });
-  
+
   try {
-    const response = await axios.get("http://localhost:8000/api/products/all-products"); // Change to GET request for all products
+    const response = await axios.get(
+      "https://urbanwear-mern-2.onrender.com/api/products/all-products"
+    );
 
     if (response.data.success) {
       dispatch({
         type: FETCH_PRODUCT_SUCCESS,
         payload: {
-          Data: response.data.data, // Assuming `data` contains the list of products
+          Data: response.data.data,
           message: response.data.message,
         },
       });
     }
   } catch (error) {
     console.error(error);
-    const message = error.response?.data?.message || "An error occurred"; // Access the error message safely
-    dispatch({ type: FETCH_PRODUCT_FAILURE, payload: message });
+
+    const message =
+      error.response?.data?.message || "An error occurred";
+
+    dispatch({
+      type: FETCH_PRODUCT_FAILURE,
+      payload: message,
+    });
   }
 };
+```
